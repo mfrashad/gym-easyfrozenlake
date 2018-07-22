a9b12a9bd0eae754a700b0cc5b23fd9a0e41aa29
mfrashad <m.fathyrashad@gmail.com>
2018-07-22 12:39:50 +0800

    change to v0.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='gym_easyfrozenlake',
-      version='0.0.1',
+      version='0.0.2',
       install_requires=['gym'],  # And any other dependencies foo needs
       url='https://github.com/mfrashad/gym_easyfrozenlake/tree/master/gym_easyfrozenlake'
 )  
