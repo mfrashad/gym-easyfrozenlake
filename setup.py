cda57209753a8193e5c30ecaf02dc31896a1f3af
mfrashad <m.fathyrashad@gmail.com>
2018-07-22 12:11:04 +0800

    add repo url pn setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import setup
 
 setup(name='gym_easyfrozenlake',
       version='0.0.1',
-      install_requires=['gym']  # And any other dependencies foo needs
+      install_requires=['gym'],  # And any other dependencies foo needs
+      url='https://github.com/mfrashad/gym-easyfrozenlake/tree/master/gym_easyfrozenlake'
 )  
